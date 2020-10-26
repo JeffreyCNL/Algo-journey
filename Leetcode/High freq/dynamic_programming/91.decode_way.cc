0ac202f45b4fca8d589fa53bb5e25e31f7a3d7c3
Jeffrey Lee <jeffreyleee3@gmail.com>
2020-10-25 21:02:06 -0700

    add another solution

--- Leetcode/High freq/dynamic_programming/91.decode_way.cc
+++ Leetcode/High freq/dynamic_programming/91.decode_way.cc
@@ -59,6 +59,29 @@ public:
         return dp.back();
     }
 };
+
+// int numDecodings(string s)
+// {
+//     if (s.empty() || s[0] == '0')
+//     {
+//         return 0;
+//     }
+//     int n = s.size();
+//     vector<int> dp(n + 1, 0);
+//     dp[0] = 1;
+//     for (int i = 1; i < dp.size(); ++i)
+//     {
+//         if (s[i - 1] != '0')
+//         {
+//             dp[i] += dp[i - 1];
+//         }
+//         if (i >= 2 && s.substr(i - 2, 2) <= "26" && s.substr(i - 2, 2) >= "10")
+//         {
+//             dp[i] += dp[i - 2];
+//         }
+//     }
+//     return dp.back();
+// }
 int main()
 {
 
